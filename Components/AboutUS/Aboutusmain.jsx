c2c4496c43448ed4e492b6428dad8754926ffd91
Aditya Morankar <47907947+adi112100@users.noreply.github.com>
2021-02-28 21:52:52 +0530

    added fontawesome  icon using import

--- Components/AboutUS/Aboutusmain.jsx
+++ Components/AboutUS/Aboutusmain.jsx
@@ -8,6 +8,8 @@ import AboutusCarousel from './AboutusCarousel';
 import AboutusCard from './AboutusCard';
 import AboutusProfile from './AboutusProfile';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPython, faNode, faReact, faAws, faJs } from '@fortawesome/free-brands-svg-icons'
 
 const Aboutusmain = () => {
 
@@ -34,14 +36,14 @@ const Aboutusmain = () => {
                         <div className="col-12 col-md-6 d-none d-md-block">
                             <img src='/Media/aboutus2.svg' />
                         </div>
-                        <div className="col-12 col-md-6 align-middle shadow " style={{textAlign:'left', padding:'15px', borderRadius:'50px'}}>
+                        <div className="col-12 col-md-6 align-middle shadow " style={{ textAlign: 'left', padding: '15px', borderRadius: '50px' }}>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque minus recusandae dolores nulla! Magni quae consequatur,
                             tenetur, odio voluptas nostrum autem reprehenderit ad delectus dolorem consectetur! Quia similique cumque maiores!
                         </div>
                     </div>
-                    <br/><br/><br/>
+                    <br /><br /><br />
                     <div className="row" >
-                        <div className="col-12 col-md-6 align-middle shadow " style={{textAlign:'left', padding:'15px', borderRadius:'50px'}}>
+                        <div className="col-12 col-md-6 align-middle shadow " style={{ textAlign: 'left', padding: '15px', borderRadius: '50px' }}>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque minus recusandae dolores nulla! Magni quae consequatur,
                             tenetur, odio voluptas nostrum autem reprehenderit ad delectus dolorem consectetur! Quia similique cumque maiores!
                         </div>
@@ -49,62 +51,65 @@ const Aboutusmain = () => {
                             <img src='/Media/aboutus3.svg' />
                         </div>
                     </div>
-                    <br/><br/><br/>
+                    <br /><br /><br />
                     <div className="row" >
-                        <h1 style={{textAlign:'center', color:'#FF9600'}}>We think and speak javascript, python or any language you really need. </h1>
-                        <div className={`col-12 ${styles.iconalign}`}>
-                        <i className="fab fa-python"></i>
-                        <i className="fab fa-node"></i>
-                        <i className="fab fa-react"></i>
-                        <i className="fab fa-aws"></i>
-                        <i className="fab fa-js"></i>
+                        <h1 style={{ textAlign: 'center', color: '#FF9600' }}>We think and speak javascript, python or any language you really need. </h1>
+                        <div className="col-12">
+                            <div className={styles.iconalign}>
+                                <FontAwesomeIcon icon={faPython} />
+                                <FontAwesomeIcon icon={faNode} />
+                                <FontAwesomeIcon icon={faReact} />
+                                <FontAwesomeIcon icon={faAws} />
+                                <FontAwesomeIcon icon={faJs} />
+                                
+                            </div>
                         </div>
                     </div>
-                    <br/><br/><br/>
+                    <br /><br /><br />
                     <div className="row">
                         <AboutusCarousel />
                     </div>
-                    <br/><br/><br/>
+                    <br /><br /><br />
                     <div className="row">
                         <div className={styles.cardalign}>
                             <AboutusCard title='UI/UX design'
-                                body= 'Don’t rely on imagination only. We can picture your product before it comes to life.'
-                                path='Media/card1.svg'/>
-                             <AboutusCard title='Server Deployment and DevOps'
-                                body= 'Don’t rely on imagination only. We can picture your product before it comes to life.'
-                                path='Media/card2.svg'/>
-                             <AboutusCard title='Software Engineering'
-                                body= 'Don’t rely on imagination only. We can picture your product before it comes to life.'
-                                path='Media/card3.svg'/>
+                                body='Don’t rely on imagination only. We can picture your product before it comes to life.'
+                                path='Media/card1.svg' />
+                            <AboutusCard title='Server Deployment and DevOps'
+                                body='Don’t rely on imagination only. We can picture your product before it comes to life.'
+                                path='Media/card2.svg' />
+                            <AboutusCard title='Software Engineering'
+                                body='Don’t rely on imagination only. We can picture your product before it comes to life.'
+                                path='Media/card3.svg' />
                         </div>
                     </div>
-                    <br/><br/><br/>
+                    <br /><br /><br />
                     <div className="row">
-                    <h1 style={{textAlign:'center', color:'#FF9600'}}>Our Team</h1>
-                    
-                    <div className={styles.profilealign} >
-                    <AboutusProfile
-                        name='Mr X'
-                        title='CEO and Co-Founder'
-                        body='ceo of xyz company.'
-                        path='Media/profile1.jpg'/>
-                    <AboutusProfile
-                        name='Mr X'
-                        title='CEO and Co-Founder'
-                        body='ceo of xyz company.'
-                        path='Media/profile1.jpg'/>
-                    <AboutusProfile
-                        name='Mr X'
-                        title='CEO and Co-Founder'
-                        body='ceo of xyz company.'
-                        path='Media/profile1.jpg'/>
-                    <AboutusProfile
-                        name='Mr X'
-                        title='CEO and Co-Founder'
-                        body='ceo of xyz company.'
-                        path='Media/profile1.jpg'/>
-                    </div>
-                    
+                        <h1 style={{ textAlign: 'center', color: '#FF9600' }}>Our Team</h1>
+
+                        <div className={styles.profilealign} >
+                            <AboutusProfile
+                                name='Mr X'
+                                title='CEO and Co-Founder'
+                                body='ceo of xyz company.'
+                                path='Media/profile1.jpg' />
+                            <AboutusProfile
+                                name='Mr X'
+                                title='CEO and Co-Founder'
+                                body='ceo of xyz company.'
+                                path='Media/profile1.jpg' />
+                            <AboutusProfile
+                                name='Mr X'
+                                title='CEO and Co-Founder'
+                                body='ceo of xyz company.'
+                                path='Media/profile1.jpg' />
+                            <AboutusProfile
+                                name='Mr X'
+                                title='CEO and Co-Founder'
+                                body='ceo of xyz company.'
+                                path='Media/profile1.jpg' />
+                        </div>
+
                     </div>
 
                 </div>
